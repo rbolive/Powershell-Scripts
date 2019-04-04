b38991b03dd6b4395c2907872e57d94c3ed8c346
rbolive <rborsboom9@outlook.com>
2019-04-04 11:46:59 +0200

    added Get-Process.ps1

---
+++ Get-Process.ps1
@@ -0,0 +1,15 @@
+﻿if ((get-process -name mspaint -ea SilentlyContinue) -eq $Null){
+    echo "Paint is not running"
+}
+else{
+    get-process -name mspaint | export-csv C:\logboek.csv 
+    Stop-Process -name mspaint
+}
+
+if ((get-process -name wordpad -ea SilentlyContinue) -eq $Null){
+    echo "Wordpad is not running"
+}
+else{
+    get-process -name wordpad | export-csv C:\logboek.csv 
+    Stop-Process -name wordpad
+}
